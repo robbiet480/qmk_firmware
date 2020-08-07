4a730213ad1cd246f4f40fad42ab1fb46d4e4446
Robbie Trencheny <me@robbiet.us>
2020-08-06 19:26:42 -0700

    Checkpoint

--- keyboards/ergodox_ez/keymaps/robbiet480/keymap.c
+++ keyboards/ergodox_ez/keymaps/robbiet480/keymap.c
@@ -78,7 +78,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 };
 
-
 extern bool g_suspend_state;
 extern rgb_config_t rgb_matrix_config;
 
@@ -196,29 +195,41 @@ uint32_t layer_state_set_user(uint32_t state) {
     default:
       break;
   }
+
+  uint8_t hid_data[32] = {0x05, layer};
+  #ifdef CONSOLE_ENABLE
+  uprintf("Sending for layer change with %u enabled layers: 0x%u\n", sizeof(keymaps)/sizeof(keymaps[0]), hid_data);
+  #endif
+
+  raw_hid_send(hid_data, 32);
+
   return state;
 };
 
 enum hid_commands
 {
   hid_version                 = 0x01,
-  hid_rgblight_enable         = 0x02,
-  hid_rgblight_disable        = 0x03,
-  hid_rgblight_toggle         = 0x04,
-  hid_rgblight_set_mode       = 0x05,
-  hid_rgblight_get_mode       = 0x06,
-  hid_rgblight_sethsv         = 0x07,
-  hid_rgblight_setrgb         = 0x08,
-  hid_rgblight_increase_val   = 0x09,
-  hid_rgblight_decrease_val   = 0x10,
-  hid_rgblight_increase_sat   = 0x11,
-  hid_rgblight_decrease_sat   = 0x12,
-  hid_rgblight_increase_hue   = 0x13,
-  hid_rgblight_decrease_hue   = 0x14,
-  hid_ergodox_led_on          = 0x15,
-  hid_ergodox_led_off         = 0x16,
-  hid_ergodox_led_brightness  = 0x17,
-  hid_get_wpm = 0x18,
+  hid_get_state               = 0x02,
+  hid_reset_qmk               = 0x03,
+  hid_get_layer_count         = 0x04,
+  hid_get_layer               = 0x05,
+  hid_set_layer               = 0x06,
+  hid_get_wpm                 = 0x07,
+  hid_lighting_enable         = 0x08,
+  hid_lighting_disable        = 0x09,
+  hid_lighting_toggle         = 0x10,
+  hid_lighting_set_mode       = 0x11,
+  hid_lighting_get_mode       = 0x12,
+  hid_lighting_sethsv         = 0x13,
+  hid_lighting_increase_hue   = 0x14,
+  hid_lighting_decrease_hue   = 0x15,
+  hid_lighting_increase_sat   = 0x16,
+  hid_lighting_decrease_sat   = 0x17,
+  hid_lighting_increase_val   = 0x18,
+  hid_lighting_decrease_val   = 0x19,
+  hid_led_on                  = 0x20,
+  hid_led_off                 = 0x21,
+  hid_led_brightness          = 0x22
 };
 
 void raw_hid_receive( uint8_t *data, uint8_t length ) {
@@ -232,42 +243,150 @@ void raw_hid_receive( uint8_t *data, uint8_t length ) {
       data[0] = 0x0;
 	    data[1] = 0x2;
 	    break;
-    case hid_rgblight_enable:
+    case hid_get_state:
+        // State spec
+        // Position 0: Always 0x02
+        // Position 1: Lighting type (0 for none, 1 for RGB matrix, 2 for RGB light)
+        // Position 2: Enabled status
+        // Position 3: Hue first bit. Hue is 0 - 360, uint8_t is 0 - 255, so we split into two entries.
+        // Position 4: Hue second bit
+        // Position 5: Sat
+        // Position 6: Val
+        // Position 7: Mode
+        // Position 8: Speed
+        // Position 9: Enabled effects (not yet implemented but will be a mask)
+        // Position 10: # of layers
+        // Position 11: Active layer
+        // Position 12: Whether WPM is enabled
+        // Position 13: Status LED position (none is 0, left is 1, right is 2, both sides is 3)
+        // gets packed values of: rgb lighting type, hsv, animation mode & speed, layers count, active layer, wpm enabled, status LED position
+        #ifdef RGB_MATRIX_ENABLE
+        data[1] = 0x01;
+        data[2] = rgb_matrix_config.enable;
+        uint8_t hue_0_255 = (rgb_matrix_config.hsv.h >> 8) & 0xFF;
+        uint8_t hue_256_360 = rgb_matrix_config.hsv.h & 0xFF;
+        data[3] = hue_0_255;
+        data[4] = hue_256_360;
+        data[5] = rgb_matrix_config.hsv.s;
+        data[6] = rgb_matrix_config.hsv.v;
+        data[7] = rgb_matrix_get_mode();
+        data[8] = rgb_matrix_config.speed;
+        data[9] = 0xFF;
+        #elif defined(RGBLIGHT_ENABLE)
+        data[1] = 0x02;
+        data[2] = rgblight_is_enabled();
+        uint8_t hue_0_255 = (rgblight_get_hue() >> 8) & 0xFF;
+        uint8_t hue_256_360 = rgblight_get_hue() & 0xFF;
+        data[3] = hue_0_255;
+        data[4] = hue_256_360;
+        data[5] = rgblight_get_sat();
+        data[6] = rgblight_get_val();
+        data[7] = rgblight_config.mode;
+        data[8] = rgblight_get_speed();
+        data[9] = 0xFF;
+        #else
+        data[1] = 0x00;
+        data[2] = 0xFF;
+        data[3] = 0xFF;
+        data[4] = 0xFF;
+        data[5] = 0xFF;
+        data[6] = 0xFF;
+        data[7] = 0xFF;
+        data[8] = 0xFF;
+        data[9] = 0xFF;
+        #endif
+
+        #ifdef CONSOLE_ENABLE
+        uprintf("Color bits: System: %u, Enabled: %u, Hue: %u, Sat: %u, Val: %u, Mode: %u, Speed: %u, Enabled effects: %u\n", data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8]);
+        #endif
+
+        data[10] = sizeof(keymaps)/sizeof(keymaps[0]);
+        data[11] = biton32(layer_state);
+        #ifdef WPM_ENABLE
+        data[12] = 0x01;
+        #else
+        data[12] = 0x00;
+        #endif
+        #ifdef LEFT_LEDS
+        data[13] = 0x01;
+        #else
+        data[13] = 0x02;
+        #endif
+
+        #ifdef CONSOLE_ENABLE
+        uprintf("Sending state %u\n", data);
+        #endif
+        break;
+    case hid_reset_qmk:
+        #ifdef CONSOLE_ENABLE
+        print("Reset!");
+        #endif
+        reset_keyboard();
+        break;
+    case hid_get_layer_count:
+        #ifdef CONSOLE_ENABLE
+        uprintf("Returning layer count of %u\n", sizeof(keymaps)/sizeof(keymaps[0]));
+        #endif
+        data[1] = sizeof(keymaps)/sizeof(keymaps[0]);
+        break;
+    case hid_get_layer:
+        #ifdef CONSOLE_ENABLE
+        uprintf("Returning active layer %u\n", biton32(layer_state));
+        #endif
+        data[1] = biton32(layer_state);
+        break;
+    case hid_set_layer:
+        #ifdef CONSOLE_ENABLE
+        uprintf("Setting active layer to %u\n", data[1]);
+        #endif
+        layer_move(data[1]);
+        break;
+    case hid_get_wpm:
+        #ifdef WPM_ENABLE
+        data[1] = get_current_wpm();
+        #ifdef CONSOLE_ENABLE
+        uprintf("Returning WPM: %u\n", data[1]);
+        #endif
+        #else
+        data[1] = 0xFF;
+        #endif
+        break;
+    case hid_lighting_enable:
         #ifdef RGB_MATRIX_ENABLE
         rgb_matrix_enable();
         #else
         rgblight_enable();
         #endif
 	    break;
-    case hid_rgblight_disable:
+    case hid_lighting_disable:
 	    #ifdef RGB_MATRIX_ENABLE
         rgb_matrix_disable();
         #else
         rgblight_disable();
         #endif
 	    break;
-    case hid_rgblight_toggle:
+    case hid_lighting_toggle:
         #ifdef RGB_MATRIX_ENABLE
         rgb_matrix_toggle();
         #else
         rgblight_toggle();
         #endif
 	    break;
-	  case hid_rgblight_set_mode:
+	  case hid_lighting_set_mode:
 	    #ifdef RGB_MATRIX_ENABLE
         rgb_matrix_mode(data[1]);
         #else
         rgblight_mode(data[1]);
         #endif
 	    break;
-	  case hid_rgblight_get_mode:
+	  case hid_lighting_get_mode:
         #ifdef RGB_MATRIX_ENABLE
         data[1] = rgb_matrix_get_mode();
         #else
         data[1] = rgblight_get_mode();
         #endif
 	    break;
-	  case hid_rgblight_sethsv:
+	  case hid_lighting_sethsv:
       {
         // Hue is 0 - 360, uint8_t is 0 - 255
         // so we split into two entries.
@@ -289,124 +408,124 @@ void raw_hid_receive( uint8_t *data, uint8_t length ) {
         #endif
         break;
       }
-    case hid_rgblight_setrgb:
-      {
-          // 1,2,3 = r,g,b
-          // 4,5,6 side,start,end
-          print("Received set RGB request\n");
-          #ifdef RGB_MATRIX_ENABLE
-          if(sizeof(data) == 5) {
-              #ifdef CONSOLE_ENABLE
-              uprintf("Setting single key RGB color via rgb_matrix: %u %u %u %u\n", data[1], data[2], data[3], data[4]);
-              #endif
-              rgb_matrix_set_color(data[1], data[2], data[3], data[4]);
-          } else {
-              #ifdef CONSOLE_ENABLE
-              uprintf("Setting all RGB colors via rgb_matrix: %u %u %u\n", data[1], data[2], data[3]);
-              #endif
-              rgb_matrix_set_color_all(data[1], data[2], data[3]);
-          }
-          #else
-          if(sizeof(data) == 5) {
-              #ifdef CONSOLE_ENABLE
-              uprintf("Setting single key RGB color via rgblight: %u %u %u %u\n", data[1], data[2], data[3], data[4]);
-              #endif
-              rgblight_setrgb_at(data[1], data[2], data[3], data[4]);
-          } else if(sizeof(data) == 6) {
-              #ifdef CONSOLE_ENABLE
-              uprintf("Setting range of key RGB colors via rgblight: %u %u %u %u %u\n", data[1], data[2], data[3], data[4], data[5]);
-              #endif
-              rgblight_setrgb_range(data[1], data[2], data[3], data[4], data[5]);
-          } else {
-              if (data[4] == 1) {
-                  #ifdef CONSOLE_ENABLE
-                  uprintf("Setting master RGB colors via rgblight: %u %u %u\n", data[1], data[2], data[3]);
-                  #endif
-                  rgblight_setrgb_master(data[1], data[2], data[3]);
-              } else if (data[4] == 2) {
-                  #ifdef CONSOLE_ENABLE
-                  uprintf("Setting slave RGB colors via rgblight: %u %u %u\n", data[1], data[2], data[3]);
-                  #endif
-                  rgblight_setrgb_slave(data[1], data[2], data[3]);
-              } else {
-                  #ifdef CONSOLE_ENABLE
-                  uprintf("Setting all RGB colors via rgblight: %u %u %u\n", data[1], data[2], data[3]);
-                  #endif
-                  rgblight_setrgb(data[1], data[2], data[3]);
-              }
-          }
-          #endif
-        break;
-      }
-    case hid_rgblight_increase_val:
-      #ifdef RGB_MATRIX_ENABLE
-      rgb_matrix_increase_val();
-      #else
-      rgblight_increase_val();
-      #endif
-      break;
-    case hid_rgblight_decrease_val:
-      #ifdef RGB_MATRIX_ENABLE
-      rgb_matrix_decrease_val();
-      #else
-      rgblight_decrease_val();
-      #endif
-      break;
-    case hid_rgblight_increase_sat:
-      #ifdef RGB_MATRIX_ENABLE
-      rgb_matrix_increase_sat();
-      #else
-      rgblight_increase_sat();
-      #endif
-      break;
-    case hid_rgblight_decrease_sat:
-      #ifdef RGB_MATRIX_ENABLE
-      rgb_matrix_decrease_sat();
-      #else
-      rgblight_decrease_sat();
-      #endif
-      break;
-    case hid_rgblight_increase_hue:
+    case hid_lighting_increase_hue:
       #ifdef RGB_MATRIX_ENABLE
       rgb_matrix_increase_hue();
       #else
       rgblight_increase_hue();
       #endif
       break;
-    case hid_rgblight_decrease_hue:
+    case hid_lighting_decrease_hue:
       #ifdef RGB_MATRIX_ENABLE
       rgb_matrix_decrease_hue();
       #else
       rgblight_decrease_hue();
       #endif
       break;
-    case hid_ergodox_led_on:
-        #ifdef CONSOLE_ENABLE
-        uprintf("Turn ON right LED %u\n", data[1]);
-        #endif
-        ergodox_right_led_on(data[1]);
+    case hid_lighting_increase_sat:
+      #ifdef RGB_MATRIX_ENABLE
+      rgb_matrix_increase_sat();
+      #else
+      rgblight_increase_sat();
+      #endif
+      break;
+    case hid_lighting_decrease_sat:
+      #ifdef RGB_MATRIX_ENABLE
+      rgb_matrix_decrease_sat();
+      #else
+      rgblight_decrease_sat();
+      #endif
+      break;
+    case hid_lighting_increase_val:
+      #ifdef RGB_MATRIX_ENABLE
+      rgb_matrix_increase_val();
+      #else
+      rgblight_increase_val();
+      #endif
+      break;
+    case hid_lighting_decrease_val:
+      #ifdef RGB_MATRIX_ENABLE
+      rgb_matrix_decrease_val();
+      #else
+      rgblight_decrease_val();
+      #endif
+      break;
+    case hid_led_on:
+        if (data[2] == 6) {
+            #ifdef CONSOLE_ENABLE
+            print("Blink all LEDs");
+            #endif
+            ergodox_blink_all_leds();
+        } else if (data[2] == 5) {
+            #ifdef CONSOLE_ENABLE
+            print("Turn ON board LED");
+            #endif
+            ergodox_board_led_on();
+        } else if (data[2] == 4) {
+            #ifdef CONSOLE_ENABLE
+            print("Turn ON all LEDs");
+            #endif
+            ergodox_led_all_on();
+        } else {
+            if (data[1] == 0) {
+                #ifdef LEFT_LEDS
+                    #ifdef CONSOLE_ENABLE
+                    uprintf("Turn ON left LED %u\n", data[2]);
+                    #endif
+                    if (data[2] == 1) {
+                        ergodox_left_led_1_on();
+                    } else if (data[2] == 2) {
+                        ergodox_left_led_2_on();
+                    } else if (data[2] == 3) {
+                        ergodox_left_led_3_on();
+                    }
+                #endif
+            } else if (data[1] == 0) {
+                #ifdef CONSOLE_ENABLE
+                uprintf("Turn ON right LED %u\n", data[2]);
+                #endif
+                ergodox_right_led_on(data[2]);
+            }
+        }
         break;
-    case hid_ergodox_led_off:
-        #ifdef CONSOLE_ENABLE
-        uprintf("Turn OFF right LED %u\n", data[1]);
-        #endif
-        ergodox_right_led_off(data[1]);
+    case hid_led_off:
+        if (data[2] == 5) {
+            #ifdef CONSOLE_ENABLE
+            print("Turn OFF board LED");
+            #endif
+            ergodox_board_led_off();
+        } else if (data[2] == 4) {
+            #ifdef CONSOLE_ENABLE
+            print("Turn OFF all LEDs");
+            #endif
+            ergodox_led_all_off();
+        } else {
+            if (data[1] == 0) {
+                #ifdef LEFT_LEDS
+                    #ifdef CONSOLE_ENABLE
+                    uprintf("Turn OFF left LED %u\n", data[2]);
+                    #endif
+                    if (data[2] == 1) {
+                        ergodox_left_led_1_off();
+                    } else if (data[2] == 2) {
+                        ergodox_left_led_2_off();
+                    } else if (data[2] == 3) {
+                        ergodox_left_led_3_off();
+                    }
+                #endif
+            } else if (data[1] == 0) {
+                #ifdef CONSOLE_ENABLE
+                uprintf("Turn OFF right LED %u\n", data[2]);
+                #endif
+                ergodox_right_led_off(data[2]);
+            }
+        }
         break;
-    case hid_ergodox_led_brightness:
+    case hid_led_brightness:
         #ifdef CONSOLE_ENABLE
         uprintf("Set right LED #%u to brightness %u\n", data[1], data[2]);
         #endif
         ergodox_right_led_set(data[1], data[2]);
-        break;
-    case hid_get_wpm:
-        #ifdef WPM_ENABLE
-        data[1] = get_current_wpm();
-        #ifdef CONSOLE_ENABLE
-        uprintf("Returning WPM: %u\n", data[1]);
-        #endif
-        #else
-        data[1] = 0xFF;
-        #endif
         break;
     default:
         data[0] = 0xFF;
