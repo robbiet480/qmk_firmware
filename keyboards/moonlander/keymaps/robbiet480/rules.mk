baf8f6a1b1b5d97d8430a246bddaf445281b787e
Robbie Trencheny <me@robbiet.us>
2020-09-25 20:03:15 -0700

    Disable console

--- keyboards/moonlander/keymaps/robbiet480/rules.mk
+++ keyboards/moonlander/keymaps/robbiet480/rules.mk
@@ -3,6 +3,21 @@
 COMMAND_ENABLE = no
 WEBUSB_ENABLE = yes
 ORYX_ENABLE = yes
+WPM_ENABLE = yes
 SRC = matrix.c
 RGBLIGHT_ENABLE = no
 RGB_MATRIX_ENABLE = yes
+RGB_HID_ENABLE            = yes
+ifeq ($(strip $(RGB_HID_ENABLE)), yes)
+	RAW_ENABLE = yes
+
+	# QMK doesn't have enough endpoints so we need to disable some features to get CONSOLE_ENABLE
+	# MOUSEKEY_ENABLE = no   # Mouse keys
+	# EXTRAKEY_ENABLE = no   # Audio control and System control
+	# NKRO_ENABLE = no	    # USB Nkey Rollover
+
+	# or
+
+	CONSOLE_ENABLE = no
+    SRC += rgb_hid_protocol.c
+endif
